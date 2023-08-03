2bc9c809cd1ddb7d4cd3089d4366ce4fb9633b27
SG-O <sg-o@live.de>
2023-08-03 08:40:35 +0200

    Added new boolean tag.

---
+++ tagyCore/src/main/java/de/sg_o/lib/tagy/tag/bool/BoolInput.java
@@ -0,0 +1,80 @@
+/*
+ *
+ *      Copyright (C) 2023 Joerg Bayer (SG-O)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sg_o.lib.tagy.tag.bool;
+
+import de.sg_o.lib.tagy.def.TagDefinition;
+import de.sg_o.lib.tagy.exceptions.InputException;
+import de.sg_o.lib.tagy.tag.Input;
+import de.sg_o.lib.tagy.tag.Tag;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BoolInput extends Input {
+    private final JCheckBox component;
+
+    public BoolInput(@NotNull TagDefinition tagDefinition) {
+        super(tagDefinition);
+        component = new JCheckBox();
+        component.setHorizontalAlignment(SwingConstants.CENTER);
+        component.setToolTipText(getTagDefinition().getKey());
+    }
+
+    public BoolInput(@NotNull Tag tag) {
+        this(tag.getDefinition());
+        reset(tag);
+    }
+
+    public @NotNull JComponent getComponent() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(Box.createHorizontalGlue(), BorderLayout.WEST);
+        panel.add(component, BorderLayout.CENTER);
+        panel.add(Box.createHorizontalGlue(), BorderLayout.EAST);
+        return panel;
+    }
+
+    @Override
+    public void reset(Tag tag) {
+        component.setSelected(false);
+        if (tag == null) return;
+        if (tag instanceof TagBool) {
+            TagBool tagBool = (TagBool) tag;
+            component.setSelected(tagBool.getValue());
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public void setValue(@NotNull Object value) {
+        if (value instanceof Boolean) {
+            component.setSelected((Boolean) value);
+        } else {
+            component.setSelected(false);
+        }
+        component.revalidate();
+        component.repaint();
+    }
+
+    @SuppressWarnings("unused")
+    public @Nullable Tag getTag() throws InputException {
+        boolean selected = component.isSelected();
+        return new TagBool(super.getTagDefinition(), selected);
+    }
+}
---
+++ tagyCore/src/main/java/de/sg_o/lib/tagy/tag/bool/TagBool.java
@@ -0,0 +1,81 @@
+/*
+ *
+ *      Copyright (C) 2023 Joerg Bayer (SG-O)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sg_o.lib.tagy.tag.bool;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import de.sg_o.lib.tagy.def.TagDefinition;
+import de.sg_o.lib.tagy.def.Type;
+import de.sg_o.lib.tagy.tag.Input;
+import de.sg_o.lib.tagy.tag.Tag;
+import de.sg_o.lib.tagy.tag.floating.DoubleInput;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class TagBool extends Tag {
+    private final boolean value;
+    public TagBool(@NotNull TagDefinition definition, boolean value) {
+        super(definition);
+        if (definition.getType() != Type.BOOLEAN) throw new IllegalArgumentException("Definition is not of type double");
+        this.value = value;
+    }
+
+    public TagBool(@NotNull TagDefinition definition, @NotNull JsonNode document) {
+        super(definition);
+        if (definition.getType() != Type.BOOLEAN) throw new IllegalArgumentException("Definition is not of type double");
+        JsonNode value = document.get("value");
+        if (value == null) throw new IllegalArgumentException("Document does not contain key");
+        this.value = value.booleanValue();
+    }
+
+    @JsonProperty(value = "value", index = 0)
+    public boolean getValue() {
+        return value;
+    }
+
+    @Override
+    public String getValueAsString() {
+        return String.valueOf(getValue());
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public Input getInputElement() {
+        return new DoubleInput(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TagBool tagDouble = (TagBool) o;
+        if (!super.definitionEquals(((Tag) o).getDefinition())) return false;
+        return Boolean.compare(tagDouble.getValue(), getValue()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.getDefinition(), getValue());
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + super.getKey() + "\": " + getValue();
+    }
+}
